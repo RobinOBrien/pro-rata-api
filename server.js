4d959bc17b3f9b8559c27d75ef36ce6d3297d9e9
Robin O'Brien <robin@valr.com>
2020-12-14 18:30:14 +0200

    Changes default port to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const errorHandler = require('./api/utils/handleErrors')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 const routes = require('./api/routes/routes')
 
